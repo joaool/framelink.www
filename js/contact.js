5019810f0208034282d3aa859ed2481089dc2ce4
Igor Kislyakov <igor_kislyakov@yahoo.com>
2018-04-11 19:33:35 +0300

    Contact Page - Added functionality

---
+++ js/contact.js
@@ -0,0 +1,25 @@
+(function (document) {
+
+    var EmailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    document.validateContactForm = function () {
+        var validName = !!$('#form-contact-email').val().length,
+            validSubject = !!$('#form-contact-subject').val().length,
+            validEmail = !!EmailRegex.test($('#form-contact-email').val()),
+            valid = validName && validSubject && validEmail;
+        $('#form-contact-submit').prop('disabled', !valid);
+        return valid;
+    };
+
+    document.onContactSendSubmit = function (e) {
+        e.preventDefault();
+        if (document.validateContactForm() && emailjs) {
+            emailjs.send('default_service', 'framelinkcontactreceiver', {
+                name: $('#form-contact-name').val(),
+                email: $('#form-contact-email').val(),
+                subject: $('#form-contact-subject').val()
+            });
+        }
+    };
+
+}(document));
