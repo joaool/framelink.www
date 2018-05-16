4eb9f3e1cf49f7d5b9c39491158f6b81712dffee
Igor Kislyakov <igor_kislyakov@yahoo.com>
2018-05-17 00:43:25 +0300

    FL Site - Fixed problem with iOS menu scrolling

--- js/common.js
+++ js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    function prevent (e) {
+        e.preventDefault();
+    }
+
     function openMobileMenu () {
         $('.main-menu-mobile').addClass('main-menu-mobile-visible');
         $('body').addClass('no-scroll');
@@ -9,6 +13,7 @@ $(document).ready(function () {
         $('body').removeClass('no-scroll');
     }
 
+    $('.main-menu-mobile').on('touchmove', prevent);
     $('.main-menu-hamburger').click(openMobileMenu);
     $('.main-menu-mobile-close').click(closeMobileMenu);
 });
