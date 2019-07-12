24ff8b660dd3a4221b7652a1f2e6d779e5ee592e
joaool <joaoccoliveira@live.com>
2019-07-12 11:17:59 +0100

    lp1_pt move to top

--- lp1_pt/assets/js/main.js
+++ lp1_pt/assets/js/main.js
@@ -3,25 +3,30 @@
 
     $(document).ready(function($){
         
-        $(".testimonial-slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: false,
-            nav: true,
-            navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>']
-        });
+        // $(".testimonial-slides").owlCarousel({
+        //     items: 1,
+        //     loop: true,
+        //     autoplay: false,
+        //     nav: true,
+        //     navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>']
+        // });
+        // $("#sticker").sticky({topSpacing:0});
+        // document.getElementById("focus").focus();
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        // alert("zzzzzzzzzzzzz");
     });
 
     $("#sticker").sticky({topSpacing:0});
 
-    $('.popup-youtube').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
-    });
+  //   $('.popup-youtube').magnificPopup({
+		// disableOn: 700,
+		// type: 'iframe',
+		// mainClass: 'mfp-fade',
+		// removalDelay: 160,
+		// preloader: false,
+		// fixedContentPos: false
+  //   });
     
     $(".hamburger-menu").on("click",function(){
         $(".mobile-menu-wrap").addClass("menu-active");
@@ -49,8 +54,15 @@
 
         setTimeout(function() {
             $('.site').addClass('loaded');
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
         }, 500);
     });
-
+    // $('#sticker').on('sticky-start', function() { 
+    //     console.log("stick force Started");
+    //     // alert("xxx"); 
+    //     document.body.scrollTop = 0; // For Safari
+    //     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    // });
 
 }(jQuery));
